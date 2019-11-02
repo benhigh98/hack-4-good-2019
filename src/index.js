8bc7665958fa04541ea411953f55df62eaf09394
Colten <coltenjohnson14@gmail.com>
2019-11-02 01:55:17 -0500

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 render (
     <BrowserRouter>
