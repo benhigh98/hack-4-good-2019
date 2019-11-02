2813c781d4d1a6d45bac9866466d1670b9fc9325
Jacob Stauch <jstauch@drury.edu>
2019-11-02 16:55:59 -0500

    Backend for extracting relevant information from a job and adding it to favorites along with travel time. Travel times will need to be created for the details page instead of for just favorites.

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,25 +1,49 @@
-import React, {Component} from 'react';
 import { Navigation } from "../components/Navigation";
-import apiKey from '../apiKey'
 import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
+import apiKey from '../apiKey';
 
 // Extract employer name, id, title, locations
 // var testJob = JSON.parse('{"data":[{"id":1,"date_posted":"2019-10-20","date_updated":"2019-11-01","date_expires":"2019-11-11","employer_id":15,"employer":{"id":15,"name":"Ankunding Ltd","category":"Ameliorated 24hour emulation","naics":362688,"created_at":"2019-11-01T22:21:09.000000Z","updated_at":"2019-11-01T22:21:09.000000Z"},"locations":{"data":[{"id":51,"name":"Professional Answering Service, Inc.","street":"3861 S. Jefferson","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"},{"id":52,"name":"Creekside at Elfindale Retirement Community","street":"1601 S. Fort","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"},{"id":53,"name":"Central Trust Company","street":"3333 S. National, 3rd Floor","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"}],"links":{"self":"\\/api\\/location"}},"title":"Outdoor Power Equipment Mechanic","job_id":668834,"pay_rate":"$55,000 per year","data_source":"Hack 4 Good Springfield","data_site":"hack4goodsgf.com","url":"https:\\/\\/hack4goodsgf.com\\/projects\\/workforce\\/?job=668834","fake":1,"created_at":"2019-11-01T22:21:21.000000Z","updated_at":"2019-11-01T22:21:21.000000Z"}]}');
 
-function jobExtract(jobObj) {
+async function jobExtract(jobObj) {
     var obj = new Object();
+    // key:curLocation
+    // value: Street Address City State (eg. 900 North Benton Ave Springfield MO)
+    var home = localStorage.getItem('curLocation');
+
     obj.employerName = jobObj.data[0]["employer"]["name"];
     obj.jobId = jobObj.data[0]["job_id"];
     obj.jobTitle = jobObj.data[0]["title"];
-    obj.jobLocations = [];
 
-    var i;
-    for (i = 0; i < jobObj.data[0]["locations"]["data"].length; i++) {
-        obj.jobLocations.push(jobObj.data[0]["locations"]["data"][i]["street"]);
-    }
+    obj.timeByCar = "";
+    obj.timeByBus = "";
+    obj.timeByBike = "";
+    obj.timeByFoot = "";
+
+    // Locations are sorted such that the closest is at the front of the list,
+    // so we can simply grab its street and construct a string acceptable for GMaps API.
+    var jobLocation = jobObj.data[0]["locations"]["data"][0]["street"] + " " +
+        jobObj.data[0]["locations"]["data"][0]["city"] + " " +
+        jobObj.data[0]["locations"]["data"][0]["state"];
+
+    const driveResponse = await fetch(`http://maps.googleapis.com/maps/api/distancematrix/json?origins={home}&destinations={jobLocation}&key={apiKey.maps}`)
+    const driveTimeObj = await driveResponse.json();
+    obj.timeByCar = driveTimeObj.rows[0]["elements"][0]["duration"]["text"];
+
+    const busResponse = await fetch(`http://maps.googleapis.com/maps/api/distancematrix/json?origins={home}&destinations={jobLocation}&mode=transit&key={apiKey.maps}`)
+    const busTimeObj = await busResponse.json();
+    obj.timeByBus = busTimeObj.rows[0]["elements"][0]["duration"]["text"];
+
+    const bikeResponse = await fetch(`http://maps.googleapis.com/maps/api/distancematrix/json?origins={home}&destinations={jobLocation}&mode=bicycling&key={apiKey.maps}`)
+    const bikeTimeObj = await bikeResponse.json();
+    obj.timeByBike = bikeTimeObj.rows[0]["elements"][0]["duration"]["text"];
+
+    const footResponse = await fetch(`http://maps.googleapis.com/maps/api/distancematrix/json?origins={home}&destinations={jobLocation}&mode=walking&key={apiKey.maps}`)
+    const footTimeObj = await footResponse.json();
+    obj.timeByFoot = footTimeObj.rows[0]["elements"][0]["duration"]["text"];
 
     var out = JSON.stringify(obj) + "[eoe]";
-
     return out;
 }
 
