f67ccd6b7e1323c9412ebdd8e096b3a78bde0a58
Ben High <bhigh002@drury.edu>
2019-11-02 11:28:25 -0500

    Add search results page

---
+++ src/apiKey.js
@@ -0,0 +1,4 @@
+export default {
+    "hack4good": "VtcA42f4HG4pYhN3sXzrTUfMlvm8ufYj24VEr1wFKvFCKyF1usIBWruhpKD3ntez8PeIJ3ab2p9G2TM3",
+    "maps": "AIzaSyAPYQ8FKRk4V7so6A4enw6hIvMkD96Ggs4"
+}
--- src/pages/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
+import apiKey from '../apiKey';
+import ReactDOM from 'react-dom'
 
-function SearchResultsPage() {
-    return (
-        <div>
+class SearchResultsPage extends Component {
+    state = {
+        done: false,
+        listings: []
+    };
 
-        </div>
-    );
+    getApiData = async function() {
+        const response = await fetch("https://jobs.api.sgf.dev/api/job?api_token=" + apiKey.hack4good);
+        const json = await response.json();
+
+        for (let listing of json.data) {
+            for (let address of listing.locations.data) {
+                let addr = (address.street + " " + address.city + " " + address.state).replace(" ", "+");
+                const googleResponse = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + escape(addr) + "&key=" + apiKey.maps);
+                const googleJson = await googleResponse.json();
+                if (googleJson["results"].length > 0) {
+                    address["lat"] = googleJson["results"][0]["geometry"]["location"]["lat"];
+                    address["lng"] = googleJson["results"][0]["geometry"]["location"]["lng"];
+                }
+            }
+        }
+
+        this.setState({listings: json.data, done: true});
+    };
+
+    componentDidMount() {
+        this.getApiData();
+    }
+
+    render() {
+        if (this.state.done) {
+            let elems = [];
+            for (let i = 0; i < this.state.listings.length; i++) {
+                elems.push(<div key={i}>{this.state.listings[i]["employer"]["name"]} - {this.state.listings[i]["title"]} - {this.state.listings[i].locations.data[0].lat}, {this.state.listings[i].locations.data[0].lng}</div>)
+            }
+
+            return <div>
+                {elems}
+            </div>
+        } else {
+            return <div>
+                <p>Loading...</p>
+            </div>
+        }
+    }
 }
 
 export default SearchResultsPage;
