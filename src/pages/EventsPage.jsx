e859aa1b88dfae3ef9a60a56d73648be06e34ee3
Colten <coltenjohnson14@gmail.com>
2019-11-02 02:35:29 -0500

    Add navigation bar

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Navigation } from "../components/Navigation";
 
 function EventsPage() {
     return (
         <div>
             <h1>Events Page</h1>
+            <Navigation />
         </div>
     );
 }
