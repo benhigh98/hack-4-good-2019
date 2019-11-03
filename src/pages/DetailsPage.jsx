655e6e16eee499b238e3f807847590614e23c884
Jacob Stauch <jstauch@drury.edu>
2019-11-02 22:21:29 -0500

    Added CSS for Favorites page, made favorite functions more JSON compliant, separated time interval functions, implemented favorites page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -6,16 +6,56 @@ import apiKey from '../apiKey';
 // Extract employer name, id, title, locations
 // var testJob = JSON.parse('{"data":[{"id":1,"date_posted":"2019-10-20","date_updated":"2019-11-01","date_expires":"2019-11-11","employer_id":15,"employer":{"id":15,"name":"Ankunding Ltd","category":"Ameliorated 24hour emulation","naics":362688,"created_at":"2019-11-01T22:21:09.000000Z","updated_at":"2019-11-01T22:21:09.000000Z"},"locations":{"data":[{"id":51,"name":"Professional Answering Service, Inc.","street":"3861 S. Jefferson","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"},{"id":52,"name":"Creekside at Elfindale Retirement Community","street":"1601 S. Fort","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"},{"id":53,"name":"Central Trust Company","street":"3333 S. National, 3rd Floor","city":"Springfield","state":"MO","zipcode":"65807","created_at":"2019-11-01T22:21:25.000000Z","updated_at":"2019-11-01T22:21:25.000000Z"}],"links":{"self":"\\/api\\/location"}},"title":"Outdoor Power Equipment Mechanic","job_id":668834,"pay_rate":"$55,000 per year","data_source":"Hack 4 Good Springfield","data_site":"hack4goodsgf.com","url":"https:\\/\\/hack4goodsgf.com\\/projects\\/workforce\\/?job=668834","fake":1,"created_at":"2019-11-01T22:21:21.000000Z","updated_at":"2019-11-01T22:21:21.000000Z"}]}');
 
-async function jobExtract(jobObj) {
+function jobExtract(jobObj) {
     var obj = new Object();
-    // key:curLocation
-    // value: Street Address City State (eg. 900 North Benton Ave Springfield MO)
-    var home = localStorage.getItem('curLocation');
+
 
     obj.employerName = jobObj.data[0]["employer"]["name"];
     obj.jobId = jobObj.data[0]["job_id"];
     obj.jobTitle = jobObj.data[0]["title"];
 
+    return obj
+}
+
+function favoriteJob(jobJSON) {
+    var favoritesList = localStorage.getItem('favorites');
+
+    if (favoritesList === null) {
+        favoritesList = JSON.stringify([jobJSON]);
+        localStorage.setItem('favorites', favoritesList);
+    }
+
+    else {
+        favoritesList = JSON.parse(favoritesList);
+        // If the job is already favorited, do nothing
+        if (JSON.stringify(favoritesList).includes(JSON.stringify(jobJSON))) {
+            void(0)
+        }
+        // Otherwise add it to the end of the string
+        else {
+            favoritesList.push(jobJSON);
+            localStorage.setItem('favorites', JSON.stringify(favoritesList));
+        }
+    }
+}
+
+function unfavoriteJob(jobJSON) {
+    var favoritesList = localStorage.getItem('favorites');
+
+    if (favoritesList.includes(JSON.stringify(jobJSON))) {
+        favoritesList = favoritesList.replace([JSON.stringify(jobJSON)],'');
+        localStorage.setItem('favorites', favoritesList);
+    }
+}
+
+// async function needs a callback to some function that updates your UI or does
+// a thing with the data
+async function getTravelTimes(jobObj) {
+    // key:curLocation
+    // value: Street Address City State (eg. 900 North Benton Ave Springfield MO)
+    var home = localStorage.getItem('curLocation');
+
+    var obj = new Object();
     obj.timeByCar = "";
     obj.timeByBus = "";
     obj.timeByBike = "";
@@ -42,40 +82,6 @@ async function jobExtract(jobObj) {
     const footResponse = await fetch(`http://maps.googleapis.com/maps/api/distancematrix/json?origins={home}&destinations={jobLocation}&mode=walking&key={apiKey.maps}`)
     const footTimeObj = await footResponse.json();
     obj.timeByFoot = footTimeObj.rows[0]["elements"][0]["duration"]["text"];
-
-    var out = JSON.stringify(obj) + "[eoe]";
-    return out;
-}
-
-function favoriteJob(jobJSON) {
-    var favoritesList = localStorage.getItem('favorites');
-
-    if (favoritesList === null) {
-        favoritesList = "";
-        favoritesList = favoritesList.concat(jobJSON);
-        localStorage.setItem('favorites', favoritesList);
-    }
-
-    else {
-        // If the job is already favorited, do nothing
-        if (favoritesList.includes(jobJSON)) {
-            void(0)
-        }
-        // Otherwise add it to the end of the string
-        else {
-            favoritesList = favoritesList.concat(jobJSON);
-            localStorage.setItem('favorites', favoritesList);
-        }
-    }
-}
-
-function unfavoriteJob(jobJSON) {
-    var favoritesList = localStorage.getItem('favorites');
-
-    if (favoritesList.includes(jobJSON)) {
-        favoritesList = favoritesList.replace(jobJSON,'');
-        localStorage.setItem('favorites', favoritesList);
-    }
 }
 
 function DetailsPage(props) {
